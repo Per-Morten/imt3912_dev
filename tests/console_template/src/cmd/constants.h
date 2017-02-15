b04c29c19a41a833b0e88c2c368d7cf74fba2535
Trond Lohne <trondloh@stud.ntnu.no>
2017-02-15 17:33:17 +0100

    ref #15: Add cmd parser files to templates
    Added the cmd parser from FTCP to both of the templates. Also had to
    change some things to get it to work properly, and removed any
    unnecessary namespace or similar things.

---
+++ tests/console_template/src/cmd/constants.h
@@ -0,0 +1,22 @@
+#pragma once
+
+namespace cmd
+{
+     namespace constants
+     {
+        constexpr auto args_from_file_cmd = "-args_from_file";
+
+        constexpr auto thread_amount_cmd = "-thread_amount";
+        constexpr auto thread_amount_default = 1;
+
+        constexpr auto test_int_cmd = "-test_int";
+        constexpr auto test_int_default = 123;
+
+        constexpr auto test_float_cmd = "-test_float";
+        constexpr auto test_float_default = 123.123f;
+
+        constexpr auto test_string_cmd = "-test_string";
+        constexpr auto test_string_default = "test";
+   }
+}
+
