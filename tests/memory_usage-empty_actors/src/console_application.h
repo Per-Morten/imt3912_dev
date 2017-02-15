b31157eab3c3163a0f5fe9b213ce43093f29ca02
Trond Lohne <trondloh@stud.ntnu.no>
2017-02-15 22:29:58 +0100

    ref #16: Make memory usage test with empty actors
    Created a simple test where a set amount of empty actors are created to
    test actor overhead.

---
+++ tests/memory_usage-empty_actors/src/console_application.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <nox/app/Application.h>
+#include <nox/app/log/Logger.h>
+#include <nox/logic/Logic.h>
+#include <nox/util/Timer.h>
+
+class ConsoleApplication: public nox::app::Application
+{
+public:
+    ConsoleApplication();
+
+    bool onInit() override;
+    void onUpdate(const nox::Duration& deltaTime) override;
+
+private:
+    bool initializeResourceCache();
+    nox::logic::Logic* initializeLogic();
+    nox::logic::physics::Simulation* initializePhysics(nox::logic::Logic* logic);
+    nox::logic::world::Manager* initializeWorldManager(nox::logic::Logic* logic);
+
+    bool loadWorldFile(nox::logic::IContext* logicContext, nox::logic::world::Manager* worldManager);
+
+    nox::app::log::Logger log;
+    nox::util::Timer<nox::Duration> outputTimer;
+
+};
