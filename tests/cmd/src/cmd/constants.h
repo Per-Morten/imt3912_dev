3f15688c45c2139e1cf3c50b01390808e7cf1c39
Trond Lohne <trondloh@stud.ntnu.no>
2017-02-21 20:19:47 +0100

    ref #16: Move cmd parser files to a single folder
    Moved the cmd parser files to a single point and created a library out
    of it to use for all the tests rather than having the files in every
    test.

---
+++ tests/cmd/src/cmd/constants.h
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace cmd
+{
+     namespace constants
+     {
+        constexpr auto args_from_file_cmd = "-args_from_file";
+
+        constexpr auto thread_amount_cmd = "-thread_amount";
+        constexpr auto thread_amount_default = 1;
+
+        constexpr auto actor_amount_cmd = "-actor_amount";
+        constexpr auto actor_amount_default = 10;
+
+        constexpr auto run_duration_ms_cmd = "-run_time_ms";
+        constexpr auto run_duration_ms_default = 1000;
+   }
+}
+
