7be98e8a1564d0767d708e02ab4f8caa4c8cdf6a
Trond Lohne <trondloh@stud.ntnu.no>
2017-02-21 20:19:19 +0100

    ref #16: Add new memory usage tests to script

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -11,16 +11,43 @@ printf "Running with 100 actors\n"
 ./build/bin/memory_usage-empty_actors -actors_amount 100 --logrm warning
 printf "\n"
 
-printf "Memory Usage - Empty Actors\n"
 printf "Running with 1000 actors\n"
 ./build/bin/memory_usage-empty_actors -actors_amount 1000 --logrm warning
 printf "\n"
 
-printf "Memory Usage - Empty Actors\n"
 printf "Running with 10000 actors\n"
 ./build/bin/memory_usage-empty_actors -actors_amount 10000 --logrm warning
+printf "\n\n"
+
+
+
+printf "Memory Usage - Physics Actors:\n"
+printf "Running with 100 actors\n"
+./build/bin/memory_usage-physics_actors -actors_amount 100 --logrm warning
 printf "\n"
 
+printf "Running with 1000 actors\n"
+./build/bin/memory_usage-physics_actors -actors_amount 1000 --logrm warning
+printf "\n"
+
+printf "Running with 10000 actors\n"
+./build/bin/memory_usage-physics_actors -actors_amount 10000 --logrm warning
+printf "\n\n"
+
+
+
+printf "Memory Usage - Sprite Actors:\n"
+printf "Running with 100 actors\n"
+./build/bin/memory_usage-sprite_actors -actors_amount 100 --logrm warning
+printf "\n"
+
+printf "Running with 1000 actors\n"
+./build/bin/memory_usage-sprite_actors -actors_amount 1000 --logrm warning
+printf "\n"
+
+printf "Running with 10000 actors\n"
+./build/bin/memory_usage-sprite_actors -actors_amount 10000 --logrm warning
+printf "\n\n"
 
 
 
