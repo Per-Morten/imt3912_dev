380281533314633daeb08addf2c6012c7add226a
Trond Lohne <trondloh@stud.ntnu.no>
2017-02-22 23:30:17 +0100

    ref #34: Add new constants to cmd constants

--- tests/cmd/src/cmd/constants.h
+++ tests/cmd/src/cmd/constants.h
@@ -10,6 +10,13 @@ namespace cmd
         constexpr auto actor_amount_cmd = "-actor_amount";
         constexpr auto actor_amount_default = 10;
 
+        constexpr auto actor_name_cmd = "-actor_name";
+        constexpr auto actor_name_default = "EmptyActor";
+
+        constexpr auto deletion_amount_cmd = "-deletion_amount";
+        constexpr auto deletion_amount_default = 1;
+
+
         constexpr auto run_duration_ms_cmd = "-run_time_ms";
         constexpr auto run_duration_ms_default = 1000;
 
