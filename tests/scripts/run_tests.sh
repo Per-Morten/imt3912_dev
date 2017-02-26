9e3ba8e914b00ff25e89d2c93e4e4aed73b6283d
Trond Lohne <trondloh@stud.ntnu.no>
2017-02-26 20:25:39 +0100

    ref #16: Change paths for new memory folder

--- tests/scripts/run_tests.sh
+++ tests/scripts/run_tests.sh
@@ -8,45 +8,45 @@ printf "Running all tests\n\n"
 
 printf "Memory Usage - Empty Actors:\n"
 printf "Running with 100 actors\n"
-./build/bin/memory_usage-empty_actors -actors_amount 100 --logrm warning
+./build/bin/empty_actors -actors_amount 100 --logrm warning
 printf "\n"
 
 printf "Running with 1000 actors\n"
-./build/bin/memory_usage-empty_actors -actors_amount 1000 --logrm warning
+./build/bin/empty_actors -actors_amount 1000 --logrm warning
 printf "\n"
 
 printf "Running with 10000 actors\n"
-./build/bin/memory_usage-empty_actors -actors_amount 10000 --logrm warning
+./build/bin/empty_actors -actors_amount 10000 --logrm warning
 printf "\n\n"
 
 
 
 printf "Memory Usage - Physics Actors:\n"
 printf "Running with 100 actors\n"
-./build/bin/memory_usage-physics_actors -actors_amount 100 --logrm warning
+./build/bin/physics_actors -actors_amount 100 --logrm warning
 printf "\n"
 
 printf "Running with 1000 actors\n"
-./build/bin/memory_usage-physics_actors -actors_amount 1000 --logrm warning
+./build/bin/physics_actors -actors_amount 1000 --logrm warning
 printf "\n"
 
 printf "Running with 10000 actors\n"
-./build/bin/memory_usage-physics_actors -actors_amount 10000 --logrm warning
+./build/bin/physics_actors -actors_amount 10000 --logrm warning
 printf "\n\n"
 
 
 
 printf "Memory Usage - Sprite Actors:\n"
 printf "Running with 100 actors\n"
-./build/bin/memory_usage-sprite_actors -actors_amount 100 --logrm warning
+./build/bin/sprite_actors -actors_amount 100 --logrm warning
 printf "\n"
 
 printf "Running with 1000 actors\n"
-./build/bin/memory_usage-sprite_actors -actors_amount 1000 --logrm warning
+./build/bin/sprite_actors -actors_amount 1000 --logrm warning
 printf "\n"
 
 printf "Running with 10000 actors\n"
-./build/bin/memory_usage-sprite_actors -actors_amount 10000 --logrm warning
+./build/bin/sprite_actors -actors_amount 10000 --logrm warning
 printf "\n\n"
 
 
