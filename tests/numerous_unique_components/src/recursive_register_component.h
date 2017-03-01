bbf0a1539bda2a9bfb71463ee40205ff6e5ad6f6
Trond Lohne <trondloh@stud.ntnu.no>
2017-03-01 12:14:44 +0100

    ref #36: Add comments to meta template functions

--- tests/numerous_unique_components/src/recursive_register_component.h
+++ tests/numerous_unique_components/src/recursive_register_component.h
@@ -4,10 +4,17 @@
 #include <components/trivial_component.h>
 
 
+// Functions for registering N amount of trivial components in the world manager.
+// It does so via meta programming, and recursively calls itself while reducing N by 1.
+
+
+// Initial function to call, is the one that recursively calls itself and counts down.
 template<std::size_t N>
 void
 registerTrivialComponent(nox::logic::world::Manager* world);
 
+// Terminate function. Template specialization when N is equal to 0.
+// Exactly the same as the other function, except that it does not call itself.
 template<>
 void
 registerTrivialComponent<0>(nox::logic::world::Manager* world);
