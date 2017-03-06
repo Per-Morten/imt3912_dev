0c732888acb3dab1935f5e8b9f4d234aa0c1d1e9
Per-Morten Straume <Gamma_ray_per@hotmail.com>
2017-03-06 20:46:15 +0100

    ref #49: Add standard component type identifiers
    Added a header for the different standard components type identifiers,
    using numerical values to avoid doing unnecessary allocations.

---
+++ ecs/src/nox/ecs/component/Types.h
@@ -0,0 +1,17 @@
+#ifndef NOX_ECS_TYPES_H_
+#define NOX_ECS_TYPES_H_
+#include <cstdint>
+
+namespace nox
+{
+    namespace ecs 
+    {
+        namespace component_types
+        {
+            constexpr std::size_t CHILDREN = 0;
+            constexpr std::size_t PARENT = 1;
+        }
+    }
+}
+
+#endif
