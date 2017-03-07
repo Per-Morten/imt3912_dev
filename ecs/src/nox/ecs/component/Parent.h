6020322485669f3bb65f848ab5063e6d2def89d7
Per-Morten Straume <Gamma_ray_per@hotmail.com>
2017-03-07 15:06:55 +0100

    ref #4: Add doxygen to Parent

--- ecs/src/nox/ecs/component/Parent.h
+++ ecs/src/nox/ecs/component/Parent.h
@@ -6,15 +6,39 @@ namespace nox
 {
     namespace ecs
     {
+        /**
+         * @brief      Component allowing for entities to query for their
+         *             parents.
+         */
         class Parent
             : public Component
         {
         public:
             using nox::ecs::Component::Component;
 
+            /**
+             * @brief      Move constructor for Parent component.
+             *
+             * @param[in]  source The component to move from.
+             */
             Parent(Parent&& other);
+
+            /**
+             * @brief      Move assignment operator for Parent component. The
+             *             function is tolerant of self assignment.
+             *
+             * @param[in]  source The component to move from.
+             *
+             * @return     *this
+             */
             Parent& operator=(Parent&& other);
 
+            /**
+             * @brief      Id of the parent to the entity owning this component.
+             * 
+             * @warning    Changing this will not have the intended effect!
+             *             The variable is public mainly to allow for move assignment.
+             */ 
             EntityId parentId{};
         };
     }
