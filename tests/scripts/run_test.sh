b42fef1d48530f2c6ca7be73548f5f9447cf0982
Trond Lohne <trondloh@stud.ntnu.no>
2017-03-07 15:40:38 +0100

    ref #63: Create run test script
    Created the run test script, which prints out various data about
    compilers, hardware, etc, along with the program output.

--- tests/scripts/run_test.sh
+++ tests/scripts/run_test.sh
@@ -1,7 +1,54 @@
 #!/bin/bash
 
-pushd ../.. > /dev/null 2>&1
+if [[ ! "$(pwd)" =~ scripts$ ]]
+then
+    printf "You need to be in the scripts directory to run this script!";
+    exit 1;
+fi
 
-/usr/bin/time --format="Test input: %C\nDuraction in seconds: %e\n" $@
 
-popd > /dev/null 2>&1
+pushd ../.. > /dev/null 2>&1;
+
+hardwareInfo="$(sudo lshw -short)";
+hardwareInfoVerbose="$(sudo lshw)";
+cpuInfo="$(sudo lscpu)";
+batteryInfo="$(upower -d)";
+cmakeCache="$(cat build/CMakeCache.txt)"
+#timingInfo="$(/usr/bin/time --format="Test input: %C\nDuraction in seconds: %e\n" $*)";
+
+startTime=$(date +%s.%N);
+programOutput="$($*)";
+endTime=$(date +%s.%N);
+duration="Duration in seconds: $(echo "$endTime - $startTime" | bc)";
+
+printf "$programOutput\n";
+
+popd > /dev/null 2>&1;
+
+
+pushd .. > /dev/null 2>&1;
+
+if [[ ! -d results ]]
+then
+    mkdir results;
+fi
+
+pushd results > /dev/null 2>&1;
+
+folderName="$(date +%F_%H-%M-%S)"
+
+mkdir "$folderName"
+
+pushd "$folderName" > /dev/null 2>&1
+
+
+echo "$hardwareInfo" >> hardwareInfoShort.txt
+echo "$hardwareInfoVerbose" >> hardwareInfoLong.txt
+echo "$cpuInfo" >> cpuInfo.txt
+echo "$batteryInfo" >> batteryInfo.txt
+echo "$programOutput" >> programOutput.txt
+echo "$cmakeCache" >> cmakeCache.txt
+echo "$duration" >> duration.txt
+
+
+popd > /dev/null 2>&1;
