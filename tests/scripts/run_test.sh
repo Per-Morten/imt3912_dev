fc0335c7bbd86b2d56a352c83a08cf42de82dcf6
Trond Lohne <trondloh@stud.ntnu.no>
2017-03-08 18:10:48 +0100

    ref #63: Clean up script and add comments

--- tests/scripts/run_test.sh
+++ tests/scripts/run_test.sh
@@ -9,25 +9,28 @@ fi
 
 pushd ../.. > /dev/null 2>&1;
 
+#Getting various hardware information
 hardwareInfo="$(sudo lshw -short)";
 hardwareInfoVerbose="$(sudo lshw)";
 cpuInfo="$(sudo lscpu)";
 batteryInfo="$(upower -d)";
-cmakeCache="$(cat build/CMakeCache.txt)"
-#timingInfo="$(/usr/bin/time --format="Test input: %C\nDuraction in seconds: %e\n" $*)";
 
-startTime=$(date +%s.%N);
-programOutput="$($*)";
-endTime=$(date +%s.%N);
-duration="Duration in seconds: $(echo "$endTime - $startTime" | bc)";
+#Copying cmake cache
+cMakeCache="$(cat build/CMakeCache.txt)"
 
-printf "$programOutput\n";
+#Running valgrind programs
+valgrind --tool=massif --time-unit=ms --detailed-freq=1 --massif-out-file=massif_output.txt --log-file=massif_log.txt $*
+valgrind --tool=callgrind --callgrind-out-file=callgrind_output.txt --log-file=callgrind_log.txt --cache-sim=yes --branch-sim=yes $*
+
+#Do time estimate of program
+{ time $* > program_output.txt ; } 2> time_output.txt;
 
 popd > /dev/null 2>&1;
 
 
 pushd .. > /dev/null 2>&1;
 
+#Create new results folder if there are none
 if [[ ! -d results ]]
 then
     mkdir results;
@@ -35,20 +38,27 @@ fi
 
 pushd results > /dev/null 2>&1;
 
+#Set folder name using date and time
 folderName="$(date +%F_%H-%M-%S)"
 
 mkdir "$folderName"
 
 pushd "$folderName" > /dev/null 2>&1
 
+#Move all outputs and logs to the appropriate folder
+mv ../../../massif_log.txt ./
+mv ../../../callgrind_log.txt ./
+mv ../../../massif_output.txt ./
+mv ../../../callgrind_output.txt ./
+mv ../../../program_output.txt ./
+mv ../../../time_output.txt ./
 
-echo "$hardwareInfo" >> hardwareInfoShort.txt
-echo "$hardwareInfoVerbose" >> hardwareInfoLong.txt
-echo "$cpuInfo" >> cpuInfo.txt
-echo "$batteryInfo" >> batteryInfo.txt
-echo "$programOutput" >> programOutput.txt
-echo "$cmakeCache" >> cmakeCache.txt
-echo "$duration" >> duration.txt
+
+echo "$hardwareInfo" >> hardware_info.txt;
+echo "$hardwareInfoVerbose" >> hardware_info_verbose.txt;
+echo "$cpuInfo" >> cpu_info.txt;
+echo "$batteryInfo" >> battery_info.txt;
+echo "$cMakeCache" >> cmakecache_copy.txt;
 
 
 popd > /dev/null 2>&1;
