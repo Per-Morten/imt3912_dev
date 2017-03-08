aca78b4bb0e327b4172ca7b92f4360ed599b1ec1
Per-Morten Straume <Gamma_ray_per@hotmail.com>
2017-03-08 20:48:12 +0100

    ref #51: Add eventTest to main.cpp
    Added a small testing function to main.cpp, however we should soon start
    adding proper tests.

--- ecs/src/tests/main.cpp
+++ ecs/src/tests/main.cpp
@@ -11,6 +11,8 @@
 #include <nox/ecs/Entity.h>
 #include <nox/ecs/Factory.h>
 #include <nox/ecs/component/Types.h>
+#include <nox/ecs/Event.h>
+#include <nox/ecs/createEventArgument.h>
 
 #include <string>
 #include <cassert>
@@ -76,7 +78,7 @@ public:
     //using nox::ecs::Component::Component;
 
     MockComponent(MockComponent&& source)
-        : nox::ecs::Component(source.id)
+        : nox::ecs::Component(source.id, source.entityManager)
         , str(std::move(source.str))
         {
         }
@@ -85,15 +87,15 @@ public:
     {
         if (this != &source)
         {
-            std::swap(id, source.id);
+            nox::ecs::Component::operator=(std::move(source));
             std::swap(str, source.str);
         }
 
         return *this;
     }
 
-    MockComponent(const nox::ecs::EntityId& id)
-        : nox::ecs::Component(id)
+    MockComponent(const nox::ecs::EntityId& id, nox::ecs::EntityManager* manager)
+        : nox::ecs::Component(id, manager)
         , str("MockComponent ")
         {
 
@@ -104,7 +106,18 @@ public:
 
     void update(const nox::Duration& deltaTime)
     {
-        printf("update %zu %s\n", this->id, str.c_str());
+        nox::ecs::TypeIdentifier type(size_t(0));
+        //printf("update %zu %s\n", this->id, str.c_str());
+        nox::ecs::Event broadcastEvent({"broadcastEvent"}, this->id);
+        auto argument = nox::ecs::createEventArgument(std::string("value"), {"Argument"});
+        broadcastEvent.addArgument({"Arg1"}, std::move(argument));
+        this->entityManager->sendEntityEvent(std::move(broadcastEvent));
+
+        nox::ecs::Event thisEvent({"thisEvent"}, this->id, this->id);
+        auto arg = nox::ecs::createEventArgument(2,{"Argument"});
+        thisEvent.addArgument({"Arg2"}, std::move(arg));
+        this->entityManager->sendEntityEvent(std::move(thisEvent));
+
     }
 
     void awake()
@@ -151,8 +164,24 @@ public:
 
     void receiveEntityEvent(const nox::ecs::Event& event)
     {
-        str += "receiveComponentEvent ";
-        PMS_DEBUG("%zu %s\n", id, str.c_str());
+        //str += "receiveComponentEvent ";
+        if (event.getType() == nox::ecs::TypeIdentifier{"broadcastEvent"})
+        {
+            auto argument = static_cast<const std::string*>(event.getArgument({"Arg1"}).value());
+            PMS_DEBUG("%zu %s\n", id, str.c_str());
+            PMS_DEBUG("Received event from: %zu, type: %zu argument: %s\n", 
+                      event.getSender(),
+                      event.getType(),
+                      argument->c_str());
+        }
+        else if (event.getType() == nox::ecs::TypeIdentifier{"thisEvent"})
+        {
+            auto argument = static_cast<const int*>(event.getArgument({"Arg2"}).value());
+            PMS_DEBUG("Received event from: %zu, type: %zu argument: %d\n",
+                      event.getSender(),
+                      event.getType(),
+                      *argument);
+        }
     }
     
 
@@ -166,7 +195,7 @@ class OtherComponent
 {
 public:
     OtherComponent(OtherComponent&& source)
-        : nox::ecs::Component(source.id)
+        : nox::ecs::Component(source.id, source.entityManager)
         , str(std::move(source.str))
         {
         }
@@ -175,15 +204,15 @@ public:
     {
         if (this != &source)
         {
-            std::swap(id, source.id);
+            nox::ecs::Component::operator=(std::move(source));
             std::swap(str, source.str);
         }
 
         return *this;
     }
 
-    OtherComponent(const nox::ecs::EntityId& id)
-        : nox::ecs::Component(id)
+    OtherComponent(const nox::ecs::EntityId& id, nox::ecs::EntityManager* manager)
+        : nox::ecs::Component(id, manager)
         , str("Other Component ")
         {
 
@@ -194,53 +223,54 @@ public:
 
     void update(const nox::Duration& deltaTime)
     {
-        printf("update %zu %s\n", this->id, str.c_str());
+        //printf("update %zu %s\n", this->id, str.c_str());
+        //this->manager->
     }
 
     void awake()
     {
         str += "awake ";
-        printf("%zu %s\n", id, str.c_str());
+        //printf("%zu %s\n", id, str.c_str());
         state = State::DEACTIVATED;
     }
 
     void activate()
     {
         str += "activate ";
-        printf("%zu %s\n", id, str.c_str());
+        //printf("%zu %s\n", id, str.c_str());
         state = State::ACTIVE;
     }
     
     void deactivate()
     {
         str += "deactivate ";
-        printf("%zu %s\n", id, str.c_str());
+        //printf("%zu %s\n", id, str.c_str());
         state = State::DEACTIVATED;
     }
 
     void hibernate()
     {
         str += "hibernate ";
-        printf("%zu %s\n", id, str.c_str());
+        ///printf("%zu %s\n", id, str.c_str());
         state = State::HIBERNATION;
     }
 
     void initialize(const Json::Value& value)
     {
         str += "initialize " + value["name"].asString();
-        printf("%zu %s\n", id, str.c_str());
+        //printf("%zu %s\n", id, str.c_str());
     }
 
     void receiveLogicEvent(const std::shared_ptr<nox::event::Event>& event)
     {
         str += "receiveLogicEvent " + event->getType() + " ";
-        printf("%zu %s\n", id, str.c_str());
+        //printf("%zu %s\n", id, str.c_str());
     }
 
     void receiveEntityEvent(const nox::ecs::Event& event)
     {
-        str += "receiveComponentEvent ";
-        printf("%zu %s\n", id, str.c_str());
+        PMS_DEBUG("I am: %zu, received from: %zu\n", this->id, event.getSender());
+        //printf("%zu %s\n", id, str.c_str());
     }
     
 
@@ -252,117 +282,117 @@ public:
 void
 test()
 {
-    using namespace nox::ecs;
-    nox::Duration duration;
+    // using namespace nox::ecs;
+    // nox::Duration duration;
 
-    const auto metaInfo = nox::ecs::createMetaInformation<MockComponent>({"MockComponent"});
-    char* buffer = new char[metaInfo.size];
+    // const auto metaInfo = nox::ecs::createMetaInformation<MockComponent>({"MockComponent"});
+    // char* buffer = new char[metaInfo.size];
 
-    Component* begin = (Component*)(buffer);
-    Component* end = (Component*)(&buffer[metaInfo.size]);
-    metaInfo.construct(begin, 1);
-    metaInfo.awake(begin);
-    metaInfo.activate(begin);
-    metaInfo.update(begin, end, duration);
-    metaInfo.deactivate(begin);
-    metaInfo.hibernate(begin);
-    metaInfo.initialize(begin, {});
-    metaInfo.receiveLogicEvent(begin, end,{});
-    metaInfo.receiveComponentEvent(begin, end,{}); 
+    // Component* begin = (Component*)(buffer);
+    // Component* end = (Component*)(&buffer[metaInfo.size]);
+    // metaInfo.construct(begin, 1);
+    // metaInfo.awake(begin);
+    // metaInfo.activate(begin);
+    // metaInfo.update(begin, end, duration);
+    // metaInfo.deactivate(begin);
+    // metaInfo.hibernate(begin);
+    // metaInfo.initialize(begin, {});
+    // metaInfo.receiveLogicEvent(begin, end,{});
+    // //metaInfo.receiveEntityEvent(begin, end,{}); 
 
-    metaInfo.destruct((Component*)buffer);
-    delete[] buffer;
+    // metaInfo.destruct((Component*)buffer);
+    // delete[] buffer;
 }
 
 void
 testingCollection()
 {
-    using namespace nox::ecs;
-    nox::Duration duration;
+    // using namespace nox::ecs;
+    // nox::Duration duration;
 
-    auto metaInfo = nox::ecs::createMetaInformation<MockComponent>({"MockComponent"});
+    // auto metaInfo = nox::ecs::createMetaInformation<MockComponent>({"MockComponent"});
 
-    metaInfo.awake;// = nullptr;
-    ComponentCollection collection(metaInfo);
-    collection.update(duration);
+    // metaInfo.awake;// = nullptr;
+    // ComponentCollection collection(metaInfo);
+    // collection.update(duration);
 
-    PMS_DEBUG("Creating\n");
-    for (std::size_t i = 0; i < 5; ++i)
-    {
-        collection.create(i);
-    }
-    printf("\n");
+    // PMS_DEBUG("Creating\n");
+    // for (std::size_t i = 0; i < 5; ++i)
+    // {
+    //     collection.create(i);
+    // }
+    // printf("\n");
 
-    PMS_DEBUG("Awaking\n");
-    for (std::size_t i = 0; i < collection.count(); ++i)
-    {
-        collection.awake(i);
-    }
-    printf("\n");
+    // PMS_DEBUG("Awaking\n");
+    // for (std::size_t i = 0; i < collection.count(); ++i)
+    // {
+    //     collection.awake(i);
+    // }
+    // printf("\n");
 
-    PMS_DEBUG("Activating\n");
-    for (std::size_t i = 0; i < collection.count(); ++i)
-    {
-        collection.activate(i);
-    }
-    printf("\n");
+    // PMS_DEBUG("Activating\n");
+    // for (std::size_t i = 0; i < collection.count(); ++i)
+    // {
+    //     collection.activate(i);
+    // }
+    // printf("\n");
 
-    PMS_DEBUG("Updating\n");
-    collection.update(duration);
-    printf("\n");
+    // PMS_DEBUG("Updating\n");
+    // collection.update(duration);
+    // printf("\n");
 
-    PMS_DEBUG("Deactivating\n");
-    for (std::size_t i = 0; i < collection.count(); ++i)
-    {
-        collection.deactivate(i);
-    }
-    printf("\n"); 
+    // PMS_DEBUG("Deactivating\n");
+    // for (std::size_t i = 0; i < collection.count(); ++i)
+    // {
+    //     collection.deactivate(i);
+    // }
+    // printf("\n"); 
 
-    PMS_DEBUG("Hibernating\n");
-    for (std::size_t i = 0; i < collection.count(); ++i)
-    {
-        collection.hibernate(i);
-    }
-    printf("\n"); 
+    // PMS_DEBUG("Hibernating\n");
+    // for (std::size_t i = 0; i < collection.count(); ++i)
+    // {
+    //     collection.hibernate(i);
+    // }
+    // printf("\n"); 
 
-    PMS_DEBUG("Removing\n");
-    for (std::size_t i = 0; i < 5; ++i)
-    {
-        collection.remove(i);
-    }
-    printf("\n"); 
+    // PMS_DEBUG("Removing\n");
+    // for (std::size_t i = 0; i < 5; ++i)
+    // {
+    //     collection.remove(i);
+    // }
+    // printf("\n"); 
 
 
-    printf("Count: %zu\n", collection.count());
+    // printf("Count: %zu\n", collection.count());
 }
 
 void
 testHandle()
 {
-    using namespace nox::ecs;
-    const auto metaInfo = nox::ecs::createMetaInformation<MockComponent>({"MockComponent"});
+    // using namespace nox::ecs;
+    // const auto metaInfo = nox::ecs::createMetaInformation<MockComponent>({"MockComponent"});
 
-    ComponentCollection collection(metaInfo);
-    collection.create(0);
+    // ComponentCollection collection(metaInfo);
+    // collection.create(0);
 
-    ComponentHandle<MockComponent> test = collection.getComponent(0);
+    // ComponentHandle<MockComponent> test = collection.getComponent(0);
 
     
-    for (std::size_t i = 1; i < 5; ++i)
-    {
-        PMS_DEBUG("Count: %zu\n", collection.count());
-        collection.create(i);
-        NOX_ASSERT(test->id == 0, "ID did not match! Iterator is not mapped to correct component");
-    }
+    // for (std::size_t i = 1; i < 5; ++i)
+    // {
+    //     PMS_DEBUG("Count: %zu\n", collection.count());
+    //     collection.create(i);
+    //     NOX_ASSERT(test->id == 0, "ID did not match! Iterator is not mapped to correct component");
+    // }
 
 
-    collection.remove(0);
+    // collection.remove(0);
 
 
 
-    collection.update({});
+    // collection.update({});
 
-    NOX_ASSERT(test.get() == nullptr, "ID is not nullptr, dangling pointer!, %zu", test->id);
+    // NOX_ASSERT(test.get() == nullptr, "ID is not nullptr, dangling pointer!, %zu", test->id);
     //printf("Passed %d", test.get() == nullptr);
     //
     //PMS_DEBUG("Capacity: %zu\n", collection.capacity() / metaInfo.size);
@@ -371,68 +401,68 @@ testHandle()
 void
 otherTest()
 {
-    using namespace nox::ecs;
-    const auto metaInfo = nox::ecs::createMetaInformation<MockComponent>({"MockComponent"});
+    // using namespace nox::ecs;
+    // const auto metaInfo = nox::ecs::createMetaInformation<MockComponent>({"MockComponent"});
 
-    ComponentCollection collection(metaInfo);
-    collection.create(0);
+    // ComponentCollection collection(metaInfo);
+    // collection.create(0);
 
-    ComponentHandle<MockComponent> test = collection.getComponent(0);
+    // ComponentHandle<MockComponent> test = collection.getComponent(0);
 
-    for (std::size_t i = 1; i < 100; ++i)
-    {
-        collection.create(i);
-        NOX_ASSERT(test->id == 0, "ID did not match! Iterator is not mapped to correct component");
-    }
+    // for (std::size_t i = 1; i < 100; ++i)
+    // {
+    //     collection.create(i);
+    //     NOX_ASSERT(test->id == 0, "ID did not match! Iterator is not mapped to correct component");
+    // }
 
-    collection.remove(0);
+    // collection.remove(0);
 
 
-    collection.update({});
+    // collection.update({});
 
-    NOX_ASSERT(test.get() == nullptr, "ID is not nullptr, dangling pointer!, %zu", test->id);
-    //printf("Passed %d", test.get() == nullptr);
-    //
+    // NOX_ASSERT(test.get() == nullptr, "ID is not nullptr, dangling pointer!, %zu", test->id);
+    // //printf("Passed %d", test.get() == nullptr);
+    // //
 }
 
 void
 testMove()
 {
-    PMS_DEBUG("Starting Move\n");
+    // PMS_DEBUG("Starting Move\n");
 
-    using namespace nox::ecs;
-    const auto metaInfo = nox::ecs::createMetaInformation<MockComponent>({"MockComponent"});
+    // using namespace nox::ecs;
+    // const auto metaInfo = nox::ecs::createMetaInformation<MockComponent>({"MockComponent"});
 
-    PMS_DEBUG("Created MetaInfo\n");
+    // PMS_DEBUG("Created MetaInfo\n");
 
-    ComponentCollection collection(metaInfo);
+    // ComponentCollection collection(metaInfo);
 
-    //PMS_DEBUG("Get component 1 MetaInfo");
-    //ComponentHandle<MockComponent> test = collection.getComponent(1);
+    // //PMS_DEBUG("Get component 1 MetaInfo");
+    // //ComponentHandle<MockComponent> test = collection.getComponent(1);
 
-    PMS_DEBUG("Creating\n");
-    for (std::size_t i = 0; i < 100; ++i)
-    {
-        //PMS_DEBUG("Create %zu\n", i);
-        //PMS_DEBUG("Capacity: %zu\n", collection.capacity() / metaInfo.size);
-        collection.update({});
-        collection.create(i);
-    }
+    // PMS_DEBUG("Creating\n");
+    // for (std::size_t i = 0; i < 100; ++i)
+    // {
+    //     //PMS_DEBUG("Create %zu\n", i);
+    //     //PMS_DEBUG("Capacity: %zu\n", collection.capacity() / metaInfo.size);
+    //     collection.update({});
+    //     collection.create(i);
+    // }
 
-    PMS_DEBUG("Removing\n");
-    collection.remove(0);
+    // PMS_DEBUG("Removing\n");
+    // collection.remove(0);
 
-    PMS_DEBUG("Before move constructor\n");
-    ComponentCollection collection2(std::move(collection));
+    // PMS_DEBUG("Before move constructor\n");
+    // ComponentCollection collection2(std::move(collection));
 
-    PMS_DEBUG("After move constructor\n");
-    collection2.update({});
+    // PMS_DEBUG("After move constructor\n");
+    // collection2.update({});
 
-    PMS_DEBUG("Before move assignment\n");
-    ComponentCollection collection3 = std::move(collection2);
+    // PMS_DEBUG("Before move assignment\n");
+    // ComponentCollection collection3 = std::move(collection2);
 
-    PMS_DEBUG("After move assignment\n");
-    collection3.update({});
+    // PMS_DEBUG("After move assignment\n");
+    // collection3.update({});
 
 }
 template<class T> 
@@ -533,11 +563,6 @@ testEntityManager()
     NOX_ASSERT(mock1stHandle.get() == nullptr,"Dangling pointer!\n");
     NOX_ASSERT(mock2ndHandle.get() == nullptr,"Dangling pointer!\n");    
     NOX_ASSERT(other1stHandle.get() != nullptr,"Wrong pointer deleted!\n");
-}
-
-void
-test(nox::ecs::Entity t)
-{
 }
 
 void
@@ -696,9 +721,59 @@ testFactory(const char* filepath)
               child1MockHandle->str.c_str());
               //child2MockHandle->id,
               //child2MockHandle->str.c_str());
+}
+
+void
+eventTest()
+{
+    using namespace nox::ecs;
+    const auto mockInfo = createMetaInformation<MockComponent>(Type::MOCK_COMPONENT);
+    const auto otherInfo = createMetaInformation<OtherComponent>(Type::OTHER_COMPONENT);
+    EntityManager manager;
+    manager.registerComponent(mockInfo);
+    manager.registerComponent(otherInfo);
+
+    auto firstId = manager.createEntity();
+    auto secondId = manager.createEntity();
+    manager.assignComponent(firstId, Type::MOCK_COMPONENT);
+    manager.assignComponent(firstId, Type::OTHER_COMPONENT);
+    manager.assignComponent(secondId, Type::OTHER_COMPONENT);
+    manager.awakeEntity(firstId);
+    manager.activateEntity(firstId);
+
+    for(size_t i = 0; i < 10; ++i)
+    {
+        manager.step({});
+    }
+
+    PMS_DEBUG("event Test over\n");
+}
 
 
+void
+eventTest()
+{
+    using namespace nox::ecs;
+    const auto mockInfo = createMetaInformation<MockComponent>(Type::MOCK_COMPONENT);
+    const auto otherInfo = createMetaInformation<OtherComponent>(Type::OTHER_COMPONENT);
+    EntityManager manager;
+    manager.registerComponent(mockInfo);
+    manager.registerComponent(otherInfo);
 
+    auto firstId = manager.createEntity();
+    auto secondId = manager.createEntity();
+    manager.assignComponent(firstId, Type::MOCK_COMPONENT);
+    manager.assignComponent(firstId, Type::OTHER_COMPONENT);
+    manager.assignComponent(secondId, Type::OTHER_COMPONENT);
+    manager.awakeEntity(firstId);
+    manager.activateEntity(firstId);
+
+    for(size_t i = 0; i < 10; ++i)
+    {
+        manager.step({});
+    }
+
+    PMS_DEBUG("event Test over\n");
 }
 
 
@@ -706,25 +781,24 @@ testFactory(const char* filepath)
  int 
  main(int argc, char** argv)
  {
-    if (argc < 2)
-    {
-        PMS_DEBUG("Enter filepath\n");
-        return 1;
-    }
+    // if (argc < 2)
+    // {
+    //     PMS_DEBUG("Enter filepath\n");
+    //     return 1;
+    // }
     // testingCollection();
     // testEntityManager();
-    // //PMS_DEBUG("Hey\n");
-    // testHandle();
-    // //PMS_DEBUG("Finish TestHandle");
-    // testingCollection();
+    //entityTest();
+    PMS_DEBUG("Event test\n");
+    eventTest();
+    //testFactory(argv[1]);
     // testHandle();
     // testMove();
     // otherTest();
-    entityTest();
     // //
     
 
-     testFactory(argv[1]);
+//     testFactory(argv[1]);
 
 	return 0;
 }
