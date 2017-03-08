663e10b64812e42b79e99042c1d5efa21a99f43e
Per-Morten Straume <Gamma_ray_per@hotmail.com>
2017-03-08 17:58:58 +0100

    ref #51: Move EventArgument into Event
    Moved the EventArgument class into the event, as that is the only place
    it is really used, also added doxygen documentation.
    
    ref #4
    ref #1

--- ecs/src/nox/ecs/Event.cpp
+++ ecs/src/nox/ecs/Event.cpp
@@ -4,9 +4,16 @@
 
 #include <nox/util/nox_assert.h>
 
-nox::ecs::Event::Event(const EntityId& receiverId,
-                       const TypeIdentifier& eventType)
-    : receiver(receiverId) 
+///////////
+// Event //
+/////////// 
+const nox::ecs::EntityId nox::ecs::Event::BROADCAST = std::numeric_limits<EntityId>::max();
+
+nox::ecs::Event::Event(const TypeIdentifier& eventType,
+                       const EntityId& senderId,
+                       const EntityId& receiverId)
+    : receiverId(receiverId) 
+    , senderId(senderId)
     , type(eventType)
 {
 
@@ -16,42 +23,43 @@ nox::ecs::Event::~Event()
 {
     for (auto& item : this->arguments)
     {
-        auto destructor = item.arg.getDestructor();
-        destructor(static_cast<EventArgument::Byte*>(item.arg.value()));
+        auto destructor = item.arg.destructor;
+        destructor(item.arg.payload);
     }
 }
 
 void
-nox::ecs::Event::addArgument(const TypeIdentifier& name,
-                             EventArgument argument)
+nox::ecs::Event::addArgument(const TypeIdentifier& identifier,
+                             Argument argument)
 {
     NOX_ASSERT(std::none_of(std::cbegin(this->arguments),
                             std::cend(this->arguments),
-                            [&name](const auto& item)
-                            { return item.name == name; }), 
+                            [&identifier](const auto& item)
+                            { return item.identifier == identifier; }), 
                "Event was given duplicate arguments!");
 
-    this->arguments.push_back({ name, std::move(argument) });
+    this->arguments.push_back({ identifier, std::move(argument) });
 }
 
 bool
-nox::ecs::Event::hasArgument(const TypeIdentifier& name) const
+nox::ecs::Event::hasArgument(const TypeIdentifier& identifier) const
 {
     return std::any_of(std::cbegin(this->arguments),
                        std::cend(this->arguments),
-                       [&name](const auto& item)
-                       { return item.name == name; });
+                       [&identifier](const auto& item)
+                       { return item.identifier == identifier; });
 }
 
-const nox::ecs::EventArgument&
-nox::ecs::Event::getArgument(const TypeIdentifier& name) const
-{
-    auto argument = std::find_if(std::cbegin(this->arguments),
-                                 std::cend(this->arguments),
-                                 [&name](const auto& item)
-                                 { return item.name == name; });
 
-    NOX_ASSERT(argument != std::cend(this->arguments), "Event has no argument with type identifier: %zu", name.getValue());
+const nox::ecs::Event::Argument&
+nox::ecs::Event::getArgument(const TypeIdentifier& identifier) const
+{
+    const auto argument = std::find_if(std::cbegin(this->arguments),
+                                       std::cend(this->arguments),
+                                       [&identifier](const auto& item)
+                                       { return item.identifier == identifier; });
+        
+    NOX_ASSERT(argument != std::cend(this->arguments), "Event has no argument with type identifier: %zu", identifier.getValue());
 
     return argument->arg;
 }
@@ -63,7 +71,63 @@ nox::ecs::Event::getType() const
 }
 
 const nox::ecs::EntityId&
+nox::ecs::Event::getSender() const
+{
+    return this->senderId;
+}
+
+const nox::ecs::EntityId&
 nox::ecs::Event::getReceiver() const
 {
-    return this->receiver;
+    return this->receiverId;
+}
+
+/////////////////////
+// Event::Argument //
+/////////////////////
+nox::ecs::Event::Argument::Argument(Byte* payload,
+                                    const TypeIdentifier& identifier,
+                                    Destructor destructor)
+    : type(identifier)
+    , payload(payload)
+    , destructor(destructor)
+{
+
+}
+
+nox::ecs::Event::Argument::Argument(Event::Argument&& source)
+    : type(source.type)
+    , payload(source.payload)
+    , destructor(source.destructor)
+{
+    source.payload = nullptr;
+    source.destructor = nullptr;
+}
+
+nox::ecs::Event::Argument&
+nox::ecs::Event::Argument::operator=(Event::Argument&& source)
+{
+    if (this != &source)
+    {
+        this->destructor(payload);
+        this->destructor = source.destructor;
+        this->payload = source.payload;
+        this->type = source.type;
+
+        source.destructor = nullptr;
+        source.payload = nullptr;
+    }
+    return *this;
+}
+
+const void* const
+nox::ecs::Event::Argument::value() const
+{
+    return this->payload;
+}
+
+const nox::ecs::TypeIdentifier&
+nox::ecs::Event::Argument::getType() const
+{
+    return this->type;
 }
--- ecs/src/nox/ecs/Event.h
+++ ecs/src/nox/ecs/Event.h
@@ -1,56 +1,246 @@
 #ifndef NOX_ECS_EVENT_H_
 #define NOX_ECS_EVENT_H_
+#include <limits>
 #include <vector>
 
 #include <nox/ecs/EntityId.h>
-#include <nox/ecs/EventArgument.h>
 #include <nox/ecs/TypeIdentifier.h>
 
 namespace nox
 {
     namespace ecs
     {
+        /**
+         * @brief      Class used for handling communication between different
+         *             entities within the ecs.
+         */
         class Event
         {
         public:
-            Event() = default; //todo: Delete this, keeping it default now for testing.
+            /**
+             * @brief      Class for holding arguments within the event.
+             *             The class is a non owning container for memory,
+             *             it is the events job to destroy all of its arguments properly.
+             */
+            class Argument
+            {
+            public:
 
-            Event(const EntityId& receiverId,
-                  const TypeIdentifier& eventType);
+                /**
+                 * @brief      Friending to get easy access to the destructor.
+                 */
+                friend ecs::Event;
 
+                /**
+                 * @brief      Doing unsigned char rather than std::uint8_t as a
+                 *             byte are not necessarily 8 bits.
+                 */
+                using Byte = unsigned char;
+
+                /**
+                 * @brief      Function pointer to allow for destruction while
+                 *             maintaining types info.
+                 */
+                using Destructor = void(*)(Byte*);
+
+                /**
+                 * @brief      Creating arguments without payload etc is illegal.
+                 */
+                Argument() = delete;
+
+                /**
+                 * @brief      Creates an argument with the given payload,
+                 *             identifier and destructor.
+                 *
+                 * @param      payload     The payload of the argument.
+                 * @param[in]  identifier  The type identifier of the type to put in
+                 *                         the argument.
+                 * @param[in]  destructor  The destructor to use when destroying the
+                 *                         argument.
+                 */
+                Argument(Byte* payload,
+                         const TypeIdentifier& identifier,
+                         Destructor destructor);
+
+                /**
+                 * @brief      Not allowing for copy construction, only movement.
+                 */
+                Argument(const Argument&) = delete;
+
+                /**
+                 * @brief      Now allowing for copy assignment, only movement.
+                 */
+                Argument& operator=(const Argument&) = delete;
+
+                /**
+                 * @brief      Move constructor of the argument.
+                 * 
+                 * @warning    source is not valid after movement.
+                 *
+                 * @param[in]  source The argument to move from.
+                 */
+                Argument(Argument&& source);
+
+                /**
+                 * @brief      Move assignment operator. Move assigns from source.
+                 *             Class is tolerant of self-assignment.
+                 *             
+                 * @warning    source is not valid after movement.
+                 *             
+                 * @param[in]  source The argument to move assign from.
+                 *
+                 * @return     *this after assignment.
+                 */
+                Argument& operator=(Argument&& source);
+
+                /**
+                 * @brief      Returns a pointer to the value held within the
+                 *             argument. The user must cast this over to the correct
+                 *             pointer.
+                 *
+                 * @note       The casting need to continue holding the const.
+                 *
+                 * @return     Pointer to value held within argument.
+                 */
+                const void* const 
+                value() const;
+
+                /**
+                 * @brief      Returns the TypeIdentifier of the argument.
+                 *
+                 * @return     The TypeIdentifier of the argument.
+                 */
+                const TypeIdentifier& 
+                getType() const;
+
+            private:
+                TypeIdentifier type;
+                Byte* payload;
+                Destructor destructor;
+            };
+
+            /**
+             * @brief      Constant value used to signal that an event shall be
+             *             broadcast, not just sent to one entity.
+             */
+            static const EntityId BROADCAST;
+           
+            /**
+             * @brief      Creating events within sender, receiver or type is
+             *             illegal.
+             */
+            Event() = delete; 
+
+            /**
+             * @brief      Constructs an event that is ready to be used and can
+             *             be given arguments.
+             *
+             * @param[in]  eventType   The event type.
+             * @param[in]  senderId    The sender of the event.
+             * @param[in]  receiverId  The receiver of the event.
+             */
+            Event(const TypeIdentifier& eventType,
+                  const EntityId& senderId,
+                  const EntityId& receiverId = nox::ecs::Event::BROADCAST);
+
+            /**
+             * @brief      Not allowing for copy construction, only movement.
+             */
             Event(const Event&) = delete;
+
+            /**
+             * @brief      Not allowing for copy assignment, only movement.
+             */
             Event& operator=(const Event&) = delete;
 
+            /**
+             * @brief      Defaulting the move constructor, should have the intended effect.
+             */
             Event(Event&&) = default;
+
+            /**
+             * @brief      Defaulting the move assignment operator, should have the intended effect.
+             */
             Event& operator=(Event&&) = default;
 
+            /**
+             * @brief      Destructor, destroys all the arguments belonging to
+             *             this event.
+             */
             ~Event();
 
+            /**
+             * @brief      Adds an argument to the event.
+             *
+             * @param[in]  identifier  The identifier of the argument.
+             * @param[in]  argument    The argument itself. It is moved from and
+             *                         is not valid after movement.
+             */
             void 
-            addArgument(const TypeIdentifier& name,
-                        EventArgument argument);
+            addArgument(const TypeIdentifier& identifier,
+                        Argument argument);
 
+            /**
+             * @brief      Checks if the event has an argument with the
+             *             identifier specified by identifier.
+             *
+             * @param[in]  identifier  The identifier of the argument to query
+             *                         for.
+             *
+             * @return     True if the event has an argument with identifier ==
+             *             identifier. False otherwise.
+             */
             bool
-            hasArgument(const TypeIdentifier& name) const;
+            hasArgument(const TypeIdentifier& identifier) const;
 
-            const EventArgument&
-            getArgument(const TypeIdentifier& name) const;
+            /**
+             * @brief      Gets the argument identified by identifier.
+             *
+             * @warning    No bounds checking is done, looking up an identifier
+             *             that does not exist is undefined behavior.
+             *
+             * @param[in]  identifier  The identifier of the argument to look
+             *                         for.
+             *
+             * @return     The argument whose identifier == identifier.
+             */
+            const Argument&
+            getArgument(const TypeIdentifier& identifier) const;
 
+            /**
+             * @brief      Returns the event type.
+             *
+             * @return     The event type.
+             */
             const TypeIdentifier& 
             getType() const;
 
+            /**
+             * @brief      Returns the sender of this event.
+             *
+             * @return     The sender of the event.
+             */
+            const EntityId&
+            getSender() const;
+
+            /**
+             * @brief      Returns the receiver of this event.
+             *
+             * @return     The receiver of the event.
+             */
             const EntityId&
             getReceiver() const;
 
         private:
             struct ArgumentPair
             {
-                TypeIdentifier name;
-                EventArgument arg;
+                TypeIdentifier identifier;
+                Argument arg;
             };
 
-            EntityId receiver;
-            TypeIdentifier type{0};
+            EntityId receiverId;
+            EntityId senderId;
+            TypeIdentifier type;
 
             std::vector<ArgumentPair> arguments;
         };
