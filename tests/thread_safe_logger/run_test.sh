4268d2aedb738dfddb8c311c58086d00290696ad
Trond Lohne <trondloh@stud.ntnu.no>
2017-03-10 21:44:15 +0100

    ref #31: Create script that runs the test
    Added a script which first runs the program, takes it output and checks
    if each line is equal to ABCDEFG...WXYZ. If every line is correct the
        test is successfull.

---
+++ tests/thread_safe_logger/run_test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+pushd ../.. > /dev/null 2>&1
+
+output="$(./build/bin/thread_safe_logger -thread_count 4 -run_time_ms 1000 --logall 2>&1)";
+
+popd > /dev/null 2>&1
+
+testString="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+IFS=$'\n';
+for line in $output;
+do
+    if [ "$line" != "$testString" ];
+    then
+        printf "Test failed!\n";  
+        printf "Line contents: $line\n";
+        exit 1;
+    fi
+done
+
+printf "Test successfull!\n";
