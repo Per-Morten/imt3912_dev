5a44476887481f15280f5ab4ee0cd1a4904b64f1
Trond Lohne <trondloh@stud.ntnu.no>
2017-03-19 13:21:55 +0100

    ref #71: Remove old tests, and add new ones

--- tests/scripts/run_all_tests.sh
+++ tests/scripts/run_all_tests.sh
@@ -1,21 +1,50 @@
 #!/bin/bash
 
+root="results";
 
 printf "Running all tests\n\n"
 
-i=0;
-while [ $i -lt 32 ];
-do
-    nr="$i";
-    if [ $i -lt 10 ];
-    then
-        nr="0$nr";
-    fi
+#i=0;
+#while [ $i -lt 32 ];
+#do
+#    nr="$i";
+#    if [ $i -lt 10 ];
+#    then
+#        nr="0$nr";
+#    fi
+#
+#    ./run_test.sh --test-name="compilation_test_$nr" --results-folder=results --command="./tests/compilation_time/run_compilation.sh" --use-massif=no --use-callgrind=no;
+#    ((i++));
+#done
 
-    ./run_test.sh --test-name="compilation_test_$nr" --results-folder=results --command="./tests/compilation_time/run_compilation.sh" --use-massif=no --use-callgrind=no;
-    ((i++));
+#Getting various hardware information
+#hardwareInfo="$(sudo lshw -short)";
+#hardwareInfoVerbose="$(sudo lshw)";
+#cpuInfo="$(sudo lscpu)";
+#batteryInfo="$(upower -d)";
+#
+##Copying cmake cache
+#cMakeCache="$(cat build/CMakeCache.txt)"
+
+
+
+i=100;
+count=0;
+while [ $count -lt 8 ];
+do
+    ./run_test.sh --test-name="empty_actors_$i" --results-folder="$root/memory_usage" --command="./build/bin/empty_actors -actor_amount $i - --logrm warning";
+    ((i=i*2));
+    ((count++));
 done
 
+i=100;
+count=0;
+while [ $count -lt 8 ];
+do
+    ./run_test.sh --test-name="physics_actors_$i" --results-folder="$root/memory_usage" --command="./build/bin/physics_actors -actor_amount $i - --logrm warning";
+    ((i=i*2));
+    ((count++));
+done
 
 
 #printf "Memory Usage - Empty Actors:\n"
