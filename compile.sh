fb9cd14cb7253e408f4cff8991969b74df14cceb
Trond Lohne <trondloh@stud.ntnu.no>
2017-03-21 17:29:28 +0100

    ref #71: Add option for cmake and make arguments

--- compile.sh
+++ compile.sh
@@ -6,12 +6,31 @@ then
 	exit 1;
 fi
 
+CMAKE_ARGUMENTS="";
+MAKE_ARGUMENTS="";
+
+#Parses all arguments and put them into variables
+for i in "$@"
+do
+    case $i in
+        --cmake-arguments=*)
+        CMAKE_ARGUMENTS="${i#*=}"
+        shift # past argument=value
+        ;;
+        --make-arguments=*)
+        MAKE_ARGUMENTS="${1#*=}"
+        shift # past argument=value
+        ;;
+    esac
+done
+
+
 if [ ! -d "./build" ]
- then
+then
 	mkdir build;
 fi
 
-pushd build > /dev/null 2>&1
-cmake ..
-make $*
-popd > /dev/null 2>&1
+pushd build > /dev/null 2>&1;
+cmake .. $CMAKE_ARGUMENTS;
+make $MAKE_ARGUMENTS;
+popd > /dev/null 2>&1;
