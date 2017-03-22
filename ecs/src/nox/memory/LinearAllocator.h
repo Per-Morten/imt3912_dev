ac298592a1f871c2eeabcab2939655764bbaa13f
Per-Morten Straume <Gamma_ray_per@hotmail.com>
2017-03-22 16:03:43 +0100

    fix #79: Create LinearAllocator
    Did some changes to the FixedSlotPool and turned it into a
    LinearAllocator that is not only for the eventsystem, but can also be
    used for event arguments.

--- ecs/src/nox/memory/LinearAllocator.h
+++ ecs/src/nox/memory/LinearAllocator.h
@@ -1,5 +1,5 @@
-#ifndef NOX_MEMORY_LOCKEDPOOL_H_
-#define NOX_MEMORY_LOCKEDPOOL_H_
+#ifndef NOX_MEMORY_LINEARALLOCATOR_H_
+#define NOX_MEMORY_LINEARALLOCATOR_H_
 #include <cstdint>
 #include <nox/memory/Byte.h>
 
@@ -8,42 +8,45 @@ namespace nox
     namespace memory
     {
         /**
-          * @brief      A memory pool that allocates memory blocks with
-          *             slotCount number of slots of slotSize size.
-          *
-          * @tparam     slotSize   How big shall each slot be.
-          * @tparam     slotCount  How many slots do we have per block.
-          */ 
-        template<std::size_t slotSize, std::size_t slotCount>
-        class FixedSlotPool
+         * @brief      A Linear allocator that allocates memory blocks with
+         *             slotCount number of slots of slotSize size. Memory is
+         *             freed upon destruction.
+         *
+         * @tparam     blockSize  How big each allocated block is.
+         */
+        template<std::size_t blockSize>
+        class LinearAllocator
         {
         public:
+            
+            static constexpr std::size_t MAX_SIZE = blockSize;
+
             /**
-             * @brief      Creates the FixedSlotPool with as many blocks as
+             * @brief      Creates the LinearAllocator with as many blocks as
              *             specified by the parameter.
              *
              * @param[in]  initialBlockCount  The initial number of blocks. Must be > 0.
              */
-            FixedSlotPool(std::size_t initialBlockCount = 1);
+            LinearAllocator(std::size_t initialBlockCount = 1);
 
             /**
              * @brief      Type is only movable.
              */
-            FixedSlotPool(const FixedSlotPool&) = delete;
-
+            LinearAllocator(const LinearAllocator&) = delete;
+            
             /**
              * @brief      Type is only movable.
              */
-            FixedSlotPool& operator=(const FixedSlotPool&) = delete;
-
+            LinearAllocator& operator=(const LinearAllocator&) = delete;
+            
             /**
              * @brief      Move constructor. Moves all members out of source.
              *
              * @warning    source will not be usable after moving. 
              * 
-             * @param[in]  source  the FixedSlotPool to move from.
+             * @param[in]  source  the LinearAllocator to move from.
              */
-            FixedSlotPool(FixedSlotPool&& source);
+            LinearAllocator(LinearAllocator&& source);
 
             /**
              * @brief      Move assignment operator. Moves all members out of
@@ -51,18 +54,18 @@ namespace nox
              *
              * @warning    source will not be usable after moving.
              *
-             * @param[in]  source  the FixedSlotPool to move from.
+             * @param[in]  source  the LinearAllocator to move from.
              *
              * @return     *this after assignment.
              */
-            FixedSlotPool& operator=(FixedSlotPool&& source);
-
+            LinearAllocator& operator=(LinearAllocator&& source);
+            
             /**
              * @brief      Destroys all blocks in the pool. The user must ensure
              *             that none of the slots holds any non destructed
              *             objects.
              */
-            ~FixedSlotPool();
+            ~LinearAllocator();
 
             /**
              * @brief      Allocates a slotSize sized memory area. The function
@@ -76,7 +79,7 @@ namespace nox
              * @return     Pointer to allocated uninitialized memory.
              */
             void* allocate(std::size_t size);
-
+            
             /**
              * @brief      Function is ignored in this allocator. The function
              *             exists only reason for easing performance testing in
@@ -86,12 +89,13 @@ namespace nox
              * @param      ptr   Ignored!
              */
             void deallocate(void* ptr) {}
-
+            
             /**
              * @brief      Resets all the slots to 0. User must ensure that
              *             delete has been called on all stored elements first.
+             *             Memory is not freed until destruction.
              */ 
-            void reset();
+            void clear();
 
         private:
             /**
@@ -113,7 +117,7 @@ namespace nox
                 /**
                  * @brief      Area of raw memory which is allocated into.
                  */
-                Byte slots[slotSize * slotCount];
+                Byte slots[MAX_SIZE];
             };
 
             /**
@@ -129,6 +133,6 @@ namespace nox
     }
 }
 
-#include <nox/memory/FixedSlotPool.tpp>
+#include <nox/memory/LinearAllocator.tpp>
 
 #endif
