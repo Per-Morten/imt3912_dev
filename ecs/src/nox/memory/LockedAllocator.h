c48d9e0aeeac3459a05a5e0db06b707943927620
Per-Morten Straume <Gamma_ray_per@hotmail.com>
2017-03-25 15:30:14 +0100

    ref #91: Add LockedAllocator
    Created a wrapper for the Allocator interface that does proper locking
    with a mutex, can be used with all allocators that uses the same
    interface as our allocators.

---
+++ ecs/src/nox/memory/LockedAllocator.h
@@ -0,0 +1,60 @@
+#ifndef NOX_MEMORY_LOCKEDALLOCATOR_H_
+#define NOX_MEMORY_LOCKEDALLOCATOR_H_
+#include <cstdint>
+#include <mutex>
+
+#include <nox/memory/Byte.h>
+
+namespace nox
+{
+    namespace memory
+    {
+        /**
+         * @brief      Basic lock wrapper for an allocator type. Basically just
+         *             forwards all functions calls into the proper allocator,
+         *             but additionally it locks with a mutex.
+         *             LockAllocator is non-movable and non-copyable because of its mutex.
+         *
+         * @tparam     Allocator  requires 3 functions.
+         *             void* allocate(std::size_t size);
+         *             void deallocate(void* ptr);
+         *             void clear();
+         */
+        template<class Allocator>
+        class LockedAllocator
+        {
+        public:
+            static constexpr std::size_t MAX_SIZE = Allocator::MAX_SIZE;
+
+            LockedAllocator() = default;
+            LockedAllocator(const LockedAllocator&) = delete;
+            LockedAllocator& operator=(const LockedAllocator&) = delete;
+            LockedAllocator(LockedAllocator&&) = delete;
+            LockedAllocator& operator=(LockedAllocator&& source) = delete;
+            ~LockedAllocator() = default;
+
+            /**
+             * @brief      Takes the mutex lock, and forwards to Allocator::allocate.
+             */
+            void* allocate(std::size_t size);
+            
+            /**
+             * @brief       Takes the mutex lock, and forwards to Allocator::deallocate.
+             */
+            void deallocate(void* ptr);
+            
+            /**
+             * @brief       Takes the mutex lock, and forwards to Allocator::clear. 
+             */ 
+            void clear();
+
+        private:
+            std::mutex mutex{};
+            Allocator allocator{};
+        };
+    }
+}
+
+#include <nox/memory/LockedAllocator.tpp>
+
+#endif
