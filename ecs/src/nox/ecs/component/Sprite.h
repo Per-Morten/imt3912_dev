6ce96abf3d50105ef633f039faf352402b6bc68b
Trond Lohne <trondloh@stud.ntnu.no>
2017-04-04 19:01:04 +0200

    ref #75: Add sprite component

---
+++ ecs/src/nox/ecs/component/Sprite.h
@@ -0,0 +1,319 @@
+#ifndef NOX_ECS_SPRITE_H_
+#define NOX_ECS_SPRITE_H_
+
+#include <nox/ecs/Component.h>
+#include <nox/ecs/SmartHandle.h>
+#include <nox/ecs/component/Transform.h>
+
+#include <glm/vec2.hpp>
+#include <glm/mat4x4.hpp>
+
+namespace nox
+{
+    namespace app
+    {
+        namespace graphics
+        {
+            class SpriteRenderNode;
+            class SceneGraphNode;
+            class TransformationNode;
+        }
+    }
+}
+
+namespace nox
+{
+    namespace ecs
+    {
+        /**
+         * @brief      Defines a sprite to be rendered. Allows the change of the
+         *             entity transform via events.
+         */
+        class Sprite final
+            : public Component
+        {
+        public:          
+            using nox::ecs::Component::Component;
+
+            /**
+             * @brief      Initializes the sprite objects member variables from
+             *             a json object
+             *
+             * @param[in]  componentJsonObject  The component json object
+             */
+            void 
+            initialize(const Json::Value& componentJsonObject);
+
+            /**
+             * @brief      Serializes the sprite objects member variables to
+             *             file via a json object
+             *
+             * @param[in]  componentObject  The component object
+             */
+            void 
+            serialize(Json::Value& componentObject);
+
+            /**
+             * @brief      Called when the component is deactivated, broadcasts
+             *             an event to the rendering system that this sprite
+             *             should not be drawn.
+             */
+            void 
+            deactivate();
+
+            /**
+             * @brief      Called when the component is activated, broadcasts
+             *             an event to the rendering system that this sprite
+             *             should be drawn.
+             */
+            void 
+            activate();    
+
+            /**
+             * @brief      Updates the transform data in the object if a change
+             *             has happened in the corresponding transform component
+             *             of the same entity.
+             *
+             * @param[in]  event  The event
+             */
+            void 
+            receiveEntityEvent(const ecs::Event& event);
+
+
+            /**
+             * @brief      Sets the center.
+             *
+             * @param[in]  center  The center
+             */
+            void 
+            setCenter(const glm::vec2& center);
+            
+            /**
+             * @brief      Sets the name of which sprite to be drawn currently.
+             *
+             * @warning    The sprite references in the spriteName parameter
+             *             needs to be registered with the addSprite function
+             *             first.
+             *
+             * @param[in]  spriteName  The sprite name
+             */
+            void 
+            setActiveSprite(const std::string& spriteName);
+        
+            /**
+             * @brief      Sets if the object should be rendered or not.
+             *
+             * @param[in]  state  if true the object will be rendered.
+             */
+            void 
+            setRenderingState(const bool state);
+        
+            /**
+             * @brief      Sets and, if necessary, updates the renderlevel of
+             *             the spriteRenderNode.
+             *
+             * @param[in]  renderLevel  Level to render sprite on. Higher value
+             *                          means closer to camera. Currently in the
+             *                          range [0, 10000]. Default 0.
+             */
+            void 
+            setRenderLevel(unsigned int renderLevel);
+
+            /**
+             * @brief      Adds a new sprite which can be used to draw from
+             *
+             * @param[in]  spriteName  The sprite name
+             */
+            void 
+            addSprite(const std::string& spriteName);
+        
+            /**
+             * @brief      Gets the color.
+             *
+             * @return     The color.
+             */
+            inline const glm::vec4& 
+            getColor() const;
+            
+            /**
+             * @brief      Gets the scale.
+             *
+             * @return     The scale.
+             */
+            inline const glm::vec2& 
+            getScale() const;
+            
+            /**
+             * @brief      Gets the center.
+             *
+             * @return     The center.
+             */
+            inline const glm::vec2& 
+            getCenter() const;
+        
+            /**
+             * Set the opacity of the Actor.
+             *
+             * This is equal to: ``` auto color = actorGraphics.getColor();
+             * color.a = opacity; actorGraphics.setColor(color); ```
+             *
+             * @param[in]  color  The color
+             * @param[in]  opacity  Opacity of the Actor in range [0, 1], where 0 is
+             *                      transparent and 1 is opaque.
+             */
+            inline void 
+            setColor(const glm::vec4& color);
+            
+            /**
+             * @brief      Sets the scale.
+             *
+             * @param[in]  scale  The scale
+             */
+            inline void 
+            setScale(const glm::vec2& scale);
+            
+            /**
+             * @brief      Sets the opacity.
+             *
+             * @param[in]  opacity  The opacity
+             */
+            inline void 
+            setOpacity(const float opacity);
+            
+            /**
+             * @brief      Sets the rotation.
+             *
+             * @param[in]  rotation  The rotation
+             */
+            inline void 
+            setRotation(const float rotation);
+            
+            /**
+             * @brief      Sets the position.
+             *
+             * @param[in]  position  The position
+             */
+            inline void 
+            setPosition(const glm::vec2& position);
+
+            /**
+             * @brief      Sets if the rendering should be flipped horizontally.
+             *
+             * @param[in]  flipped  True if setting to flipped.
+             */
+            inline void 
+            setFlippedHorizontal(const bool flipped);
+        
+            /**
+             * @brief      Sets if the rendering should be flipped vertically.
+             *
+             * @param[in]  flipped  True if setting to flipped.
+             */
+            inline void 
+            setFlippedVertical(const bool flipped);
+
+            /**
+             * @brief      Sets the offset.
+             *
+             * @param[in]  offset  The offset
+             */
+            inline void 
+            setOffset(const glm::vec2& offset);    
+
+        private:
+            
+            /**
+             * @brief      Creates all object necessary for the sprite object to
+             *             function properly. Is not placed in the constructor
+             *             as it needs to know about other components that is
+             *             not created until the initializers are run.
+             */
+            void 
+            onCreate();
+
+            /**
+             * @brief      Called when the color of the render component has
+             *             changed.
+             *
+             * @param      color  The new color.
+             */
+            void 
+            onColorChange(const glm::vec4& color);
+            
+            /**
+             * @brief      Called when the light multiplier has changed.
+             *
+             * @param[in]  lightMultiplier  The light multiplier
+             */
+            void 
+            onLightMultiplierChange(const float lightMultiplier);
+            
+
+            /**
+             * @brief      Called when the emissive light has changed.
+             *
+             * @param[in]  emissiveLight  The emissive light
+             */
+            void 
+            onEmissiveLightChange(const float emissiveLight);
+
+            /**
+             * @brief      Creates a scene node.
+             *
+             * @return     The new scene node.
+             */
+            std::shared_ptr<nox::app::graphics::SceneGraphNode> 
+            createSceneNode() const;
+            
+            /**
+             * @brief      Called when certain member variables are changed, and
+             *             broadcasts those changes to the nox rendering system.
+             */
+            void 
+            updateRenderTransform();
+            
+            /**
+             * @brief      Called when certain member variables are changed, and
+             *             broadcasts those changes to the nox rendering system.
+             */
+            void 
+            updateEntityTransform();
+            
+            /**
+             * @brief      Broadcasts a scene node creation.
+             */
+            void 
+            broadcastSceneNodeCreation();
+            
+            /**
+             * @brief      Broadcasts a scene node removal.
+             */
+            void 
+            broadcastSceneNodeRemoval();
+
+            glm::vec4 color{};
+            glm::vec2 position{};
+            glm::vec2 scale{};
+            glm::vec2 flipScale{};
+            glm::vec2 offset{};
+            glm::vec2 center{};
+
+            ComponentHandle<Transform> transformHandle{};
+            std::shared_ptr<app::graphics::TransformationNode> entityTransformNode{};
+            std::shared_ptr<app::graphics::TransformationNode> renderTransformNode{};
+            std::shared_ptr<nox::app::graphics::SpriteRenderNode> spriteRenderNode{};
+
+            float rotation{};
+            float lightMultiplier{};
+            float emissiveLight{};
+            unsigned int renderLevel{};
+            std::string spriteName{};
+            bool renderingEnabled{};
+        
+        };
+    }
+}
+
+#include <nox/ecs/component/Sprite.ipp>
+
+#endif
---
+++ ecs/src/nox/ecs/component/Sprite.ipp
@@ -0,0 +1,82 @@
+const glm::vec4& 
+nox::ecs::Sprite::getColor() const
+{
+    return this->color;
+}
+
+const glm::vec2& 
+nox::ecs::Sprite::getScale() const
+{
+    return this->scale;
+}
+
+const glm::vec2& 
+nox::ecs::Sprite::getCenter() const
+{
+    return this->center;
+}
+
+void 
+nox::ecs::Sprite::setColor(const glm::vec4& color)
+{
+    this->color = color;
+    this->onColorChange(this->color);
+}
+
+void 
+nox::ecs::Sprite::setScale(const glm::vec2 &scale)
+{
+    if (this->scale != scale)
+    {
+        this->scale = scale;
+        this->updateRenderTransform();
+    }
+}
+
+void 
+nox::ecs::Sprite::setOpacity(const float opacity)
+{
+    this->color.a = opacity;
+    this->onColorChange(this->color);
+}
+
+void 
+nox::ecs::Sprite::setRotation(const float rotation)
+{
+    if (this->rotation != rotation)
+    {
+        this->rotation = rotation;
+        this->updateRenderTransform();
+    }
+}
+
+void 
+nox::ecs::Sprite::setPosition(const glm::vec2& position)
+{
+    if (this->position != position)
+    {
+        this->position = position;
+        this->updateRenderTransform();
+    }
+}
+
+void 
+nox::ecs::Sprite::setFlippedHorizontal(const bool flipped)
+{
+    this->flipScale.x = (flipped == true) ? -1.0f : 1.0f;
+    this->updateRenderTransform();
+}
+
+void 
+nox::ecs::Sprite::setFlippedVertical(const bool flipped)
+{
+    this->flipScale.y = (flipped == true) ? -1.0f : 1.0f;
+    this->updateRenderTransform();
+}
+
+void 
+nox::ecs::Sprite::setOffset(const glm::vec2& offset)
+{
+    this->offset = offset;
+    this->updateEntityTransform();
+}
